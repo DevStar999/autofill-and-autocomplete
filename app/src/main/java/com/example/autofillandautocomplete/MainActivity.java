ca184f0fbd65d85ba96033ef1e364b8e8d6b71a5
DevStar999 <wolverinerage99@gmail.com>
2021-06-29 07:54:28 +0530

    Completed implementation of autocomplete text view

--- app/src/main/java/com/example/autofillandautocomplete/MainActivity.java
+++ app/src/main/java/com/example/autofillandautocomplete/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.autofillandautocomplete;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Getting access to our autocomplete text view
+        AutoCompleteTextView autoCompleteTextView = findViewById(R.id.autoCompleteTextView);
+
+        // Creating some content for the text view
+        List<String> narutoCharacters = new ArrayList<String>() {{
+            add("Naruto"); add("Jiraiya"); add("Minato"); add("Rock Lee"); add("Sasuke");
+            add("Nina"); add("Noel"); add("Nire");
+        }};
+
+        // Creating an ArrayAdapter for our text view
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, narutoCharacters);
+
+        // Setting the adapter to our text view
+        autoCompleteTextView.setAdapter(arrayAdapter);
     }
 }
